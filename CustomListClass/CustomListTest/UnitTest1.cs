81aa0004d7399a9c52b743d6493757141c2a8750
dana <danaw09@gmail.com>
2019-07-10 11:58:06 -0500

    added more classes and added <T> class

--- CustomListClass/CustomListTest/UnitTest1.cs
+++ CustomListClass/CustomListTest/UnitTest1.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClass;
+using System.Collections.Generic;
 
 namespace CustomListTest
 {
     [TestClass]
-    public class UnitTest1
+    public class CustomClassTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Custom_List_Adds_String_To_Empty_List()
         {
+            // arrange
+            CustomList<string> customList = new CustomList<string>();
+            string inputValue = "String 1";
+
+            // act
+            customList.Add(inputValue);
+
+            // assert
+            Assert.AreSame(customList[0], inputValue);
+
         }
     }
 }
