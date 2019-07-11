4c5ea3eaadc0b1f763e6303b386b620a65fcab17
dana <danaw09@gmail.com>
2019-07-10 22:13:25 -0500

    added more unit testing to the unit test to add methods

--- CustomListClass/CustomListTest/UnitTest1.cs
+++ CustomListClass/CustomListTest/UnitTest1.cs
@@ -9,21 +9,42 @@ namespace CustomListTest
     public class CustomClassTest
     {
         [TestMethod]
-        public void Custom_List_Adds_String_To_Empty_List()
-        {
-            // arrange
-            CustomList<string> customList = new CustomList<string>();
-            string inputValue = "1";
 
-            // act
-            customList.Add(inputValue);
+
+
+            public void Adds_String_Checks_Count_For_Three()
+            {
+                // arrange
+                CustomList<string> customList = new CustomList<string>();
+                string inputValue1 = "one";
+                string inputValue2 = "two";
+                string inputValue3 = "three";
+                int expectedCount = 3;
+
+                // act
+                customList.Add(inputValue1);
+                customList.Add(inputValue2);
+                customList.Add(inputValue3);
+                int actualCount = customList.Count;
 
             // assert
-            Assert.AreSame(customList[0], inputValue);
+            Assert.AreEqual(expected, actual);
+           }
 
+        public void Add_AddToEmptyList_ValueGoesToIndexZero()
+        {
+            // arrange
+            CustomList<int> test = new CustomList<int>();
+            int expected = 10;
+            int actual;
+            // act
+            test.Add(10);
+            actual = test[0];
+            // assert
+            Assert.AreEqual(expected, actual);
         }
 
-        public void Custom_List_Adds_String_Checks_Count_For_One()
+        public void Adds_String_Checks_Count_For_One()
         {
             // arrange
             CustomList<string> customList = new CustomList<string>();
@@ -32,10 +53,15 @@ namespace CustomListTest
 
             // act
             customList.Add(inputValue);
-            int actualCount = customList.Count;
+            int actual = customList.Count;
 
             // assert
-            Assert.AreEqual(expectedCount, actualCount);
+            Assert.AreEqual(expected, actual);
+
+
+
+
+
 
         }
     }
